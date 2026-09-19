0001ef74b4691f1aa55d0310a3ce0e390e5606aa
Parvez Aman <parvezaman212@gmail.com>
2026-09-19 23:38:33 +0530

    fix(hooks): keep clean com.automation.playwright.hooks glue

--- src/test/java/com/demoqa/playwright/runners/PlaywrightTestRunner.java
+++ src/test/java/com/demoqa/playwright/runners/PlaywrightTestRunner.java
@@ -8,8 +8,7 @@ import org.testng.annotations.DataProvider;
         features = "src/test/resources/features",
         glue = {
                 "com.demoqa.playwright.stepdefinitions",
-                "com.automation.playwright.hooks",
-                "com.automation.hooks.playwright"
+                "com.automation.playwright.hooks"
         },
         plugin = {
                 "pretty",
